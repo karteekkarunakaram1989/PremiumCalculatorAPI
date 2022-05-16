87dc14a698b1b219880519d7d954045ac162b7bc
Karteek Karunakaram <kkarunakaram@australianfiftyleaders.com>
2022-05-16 21:24:41 +1000

    Added input model and common data required for calculating premium

---
+++ Services/CommonData/OccupationRating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.CommonData
+{
+    public static class OccupationRating
+    {
+        public const string Professional = "Professional";
+        public const string White_Collar = "White Collar";
+        public const string Light_Manual = "Light Manual";
+        public const string Heavy_Manual = "Heavy Manual";
+    }
+
+    public static class OccupationRatingFactor
+    {
+        public const double Professional = 1.0;
+        public const double White_Collar = 1.25;
+        public const double Light_Manual = 1.5;
+        public const double Heavy_Manual = 1.75;
+    }
+}
---
+++ Services/Models/CustomerDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Models
+{
+    public class CustomerDetails
+    {
+        public int Age { get; set; }
+        public string OccupationRating { get; set; }
+        public double SumInsured { get; set; }
+    }
+}
