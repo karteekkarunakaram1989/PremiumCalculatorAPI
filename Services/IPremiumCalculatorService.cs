74b77f6354f6ac66e2495d4ae3cefcd8ade8ebac
Karteek Karunakaram <kkarunakaram@australianfiftyleaders.com>
2022-05-17 12:02:09 +1000

    Added logic to calculate premium

---
+++ Services/IPremiumCalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IPremiumCalculatorService
+    {
+        public Task<double> CalculatePremium(CustomerDetails customerDetails);
+    }
+}
---
+++ Services/PremiumCalculatorService.cs
@@ -0,0 +1,30 @@
+﻿using Services.CommonData;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class PremiumCalculatorService : IPremiumCalculatorService
+    {
+        public async Task<double> CalculatePremium(CustomerDetails customerDetails)
+        {
+            var occupationRatingFactor = GetOccupationRatingFactor(customerDetails.OccupationRating);
+            return customerDetails.SumInsured * occupationRatingFactor * customerDetails.Age / 1000 * 12;
+        }
+
+        private double GetOccupationRatingFactor(string occupationRating)
+        {
+            switch (occupationRating)
+            {
+                case OccupationRating.Professional: return OccupationRatingFactor.Professional;
+                case OccupationRating.White_Collar: return OccupationRatingFactor.White_Collar;
+                case OccupationRating.Light_Manual: return OccupationRatingFactor.Light_Manual;
+                case OccupationRating.Heavy_Manual: return OccupationRatingFactor.Heavy_Manual;
+                default: throw new Exception("Invalid Occupation Rating");
+            }
+        }
+    }
+}
