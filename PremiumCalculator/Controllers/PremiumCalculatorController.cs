c31ed25555a09a9694760e6f6eaab4d0e6108317
Karteek Karunakaram <kkarunakaram@australianfiftyleaders.com>
2022-05-17 14:06:18 +1000

    Removed the API project and added an angular project which adds the api within itself

---
+++ PremiumCalculator/Controllers/PremiumCalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services;
+using Services.Models;
+
+namespace PremiumCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremiumCalculatorController : ControllerBase
+    {
+        #region Properties
+
+        private readonly ILogger<PremiumCalculatorController> _logger;
+        private readonly IPremiumCalculatorService _premiumCalculatorService;
+
+        #endregion
+
+        #region Constructor
+
+        public PremiumCalculatorController(ILogger<PremiumCalculatorController> logger, IPremiumCalculatorService premiumCalculatorService)
+        {
+            _logger = logger;
+            _premiumCalculatorService = premiumCalculatorService;
+        }
+
+        #endregion
+
+        [HttpGet]
+        [Route("getCalculatedPremium")]
+        public async Task<ActionResult<double>> CalculatePremium(CustomerDetails customerDetails)
+        {
+            try
+            {
+                var calculatedPremium = await _premiumCalculatorService.CalculatePremium(customerDetails);
+                return Ok(calculatedPremium);
+            }
+            catch (Exception ex)
+            {
+                // Logging to Default for now.
+                _logger.LogError($"Something went wrong inside the CalculatePremium action: {ex}");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong in the server while calculating the premium.");
+            }
+        }
+    }
+}
---
+++ PremiumCalculator/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PremiumCalculator.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureUserCreatedServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPremiumCalculatorService, PremiumCalculatorService>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                  builder => builder.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
+            });
+        }
+    }
+}
